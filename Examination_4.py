7eff734f6b1fa95ec11ccf962403cf1abbbfe10d
natchaphonexe-create <natchaphonexe@gmail.com>
2026-09-17 16:23:09 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-weight = float(input())
+weight = float(input("กรอกน้ำหนัก"))
+if weight: <= 1
+   print ("จ่าย 30 ")
+elif weight:<= 5
+   print ("จ่าย 50")
+else weight: > 5
+   print ("จ่าย 100")
